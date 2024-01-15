3aca9630ea8e28eb1ca3ad155cb9f4305255ebf7
Janis Schiller <schillerjanis@gmail.com>
2024-01-15 19:43:53 +0100

    update

--- pages/api/providers/index.js
+++ pages/api/providers/index.js
@@ -1,7 +1,7 @@
 import dbConnect from "@/db/connect.js";
 import Provider from "@/db/models/Provider.js";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/pages/api/providers/auth/[...nextauth]";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export default async function handler(request, response) {
   await dbConnect();
