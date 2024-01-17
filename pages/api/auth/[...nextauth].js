babccfaaebb3db536be3ef302567b49e825740f8
Janis Schiller <schillerjanis@gmail.com>
2024-01-17 11:12:12 +0100

    update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,23 +9,6 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "username" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (
-          credentials.username === "fisch" &&
-          credentials.password === "fisch"
-        ) {
-          return { name: "Neuer Fisch", email: "test@example.com" };
-        } else {
-          return null;
-        }
-      },
-    }),
     // ...add more providers here
   ],
 };
