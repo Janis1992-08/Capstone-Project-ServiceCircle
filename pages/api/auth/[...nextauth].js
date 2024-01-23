c88e725d8f6f37e715c773821b3b3a3d5f197644
Janis Schiller <schillerjanis@gmail.com>
2024-01-23 17:03:49 +0100

    removing Credentials

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -8,27 +7,6 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-    }),
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "username" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (
-          (credentials.username === "fisch" &&
-            credentials.password === "fisch") ||
-          (credentials.username === "test" && credentials.password === "test")
-        ) {
-          return {
-            name: credentials.username,
-            email: `${credentials.username}@example.com`,
-          };
-        } else {
-          return null;
-        }
-      },
     }),
     // ...add more providers here
   ],
